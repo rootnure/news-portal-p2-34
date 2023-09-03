0e675df86f5ad8b89b652a2a9d8f131362b65452
rootnure <nur.diu.2791@gmail.com>
2023-09-03 07:58:23 +0600

    Dynamic Categories added

--- js/news.js
+++ js/news.js
@@ -1,1 +1,19 @@
-console.log('news.js hi ditechey');
+const loadAllCategories = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
+    const data = await res.json();
+    const newsCategory = data.data.news_category;
+    displayAllCategory(newsCategory);
+}
+
+const displayAllCategory = categories => {
+    const categoryContainer = document.getElementById('category-container');
+    categories.forEach(category => {
+        const li = document.createElement('li');
+        li.classList = ` px-3 py-1 rounded-md cursor-pointer`;
+        li.setAttribute('onclick', `loadNewsByCategory('${category.category_id}')`);
+        li.innerText = `${category.category_name}`;
+        categoryContainer.appendChild(li);
+    });
+}
+
+loadAllCategories();
