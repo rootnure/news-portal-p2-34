e11faaf6b85f2ca496c8d48fd45e0864917f8547
rootnure <nur.diu.2791@gmail.com>
2023-09-03 09:07:34 +0600

    Sorting and Result summary section added. All News API test success

--- js/news.js
+++ js/news.js
@@ -9,11 +9,25 @@ const displayAllCategory = categories => {
     const categoryContainer = document.getElementById('category-container');
     categories.forEach(category => {
         const li = document.createElement('li');
-        li.classList = ` px-3 py-1 rounded-md cursor-pointer`;
+        li.classList = `px-2 py-1 rounded-md cursor-pointer`;
         li.setAttribute('onclick', `loadNewsByCategory('${category.category_id}')`);
         li.innerText = `${category.category_name}`;
         categoryContainer.appendChild(li);
     });
 }
+
+const loadNewsByCategory = async (categoryId = '08') => {
+    const apiUrl = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
+    const res = await fetch(apiUrl);
+    const data = await res.json();
+    const allNews = data.data;
+    displayCategoryNews(allNews);
+}
+
+const displayCategoryNews = allNews => {
+    console.log(allNews);
+}
+
+loadNewsByCategory('01');
 
 loadAllCategories();
